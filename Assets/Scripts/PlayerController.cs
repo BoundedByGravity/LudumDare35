fd9368e1c122ea3ba62955bcc80b2fd1773a5f79
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-04-16 18:21:32 +0200

    PlayerController now working and asset files in text instead of binary

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	Rigidbody body;
-	float moveSpeed = 20.0f;
+	float radius = 26;
+	Vector3 trajectory = new Vector3 (0, 0, 1);
+	float moveSpeed = 2.0f;
 	//Vector3 prevAddedMoveVelocity;
 	//GravitySink gravitySink;
 
@@ -19,31 +21,60 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	bool isPlanetbound() {
 		float dist = Vector3.Distance (body.position, Vector3.zero);
 		// TODO: Use radius from sphere
-		return dist < 30;
+		return dist < radius+10;
+	}
+
+	void land() {
+		body.position = body.position.normalized * radius;
 	}
 
 	void FixedUpdate() {
 		float horizontal = Input.GetAxis ("Horizontal");
 		float vertical = Input.GetAxis ("Vertical");
 
-		float alpha = horizontal;
-		float beta = vertical;
+		float degree_rotation = 3;
 
-		body.rotation = Quaternion.FromToRotation(Vector3.up, body.position);
-		body.position = body.position.normalized * 26;
-		
-		if (isPlanetbound()) {
-			Vector3 vec = new Vector3((body.position.y + body.position.z) * alpha, 
-			                          -body.position.x * alpha - body.position.z * beta, 
-			                          (body.position.x + body.position.y)* beta).normalized;
-			//Vector3 direction = new Vector3 (horizontal, 0, vertical).normalized;
-			body.velocity = vec * moveSpeed;
+		if (isPlanetbound ()) {
+			// Måns is love, Måns is life
+			if (horizontal != 0) {
+				Vector3 trajectory2 = -horizontal * Vector3.Cross(trajectory, body.position.normalized);
+				trajectory = trajectory*Mathf.Cos (degree_rotation*Mathf.PI/180) + trajectory2*Mathf.Sin(degree_rotation*Mathf.PI/180);
+			}
+			if (vertical != 0) {
+				body.position += (moveSpeed * vertical) * trajectory;
+				trajectory = (trajectory - Vector3.Project (trajectory, body.position)).normalized;
+			}
+			//body.rotation = Quaternion.FromToRotation(Vector3.forward, trajectory);
+			Debug.Log (Vector3.Dot (trajectory, body.position));
+			body.gameObject.transform.rotation = Quaternion.LookRotation(trajectory, body.position);
+			Debug.Log (body.gameObject.transform.rotation);
+			land ();
+		} else {
+			// If in space
 		}
+	}
+
+	void MovePlayer1(float vertical, float horizontal) {
+		// Broken	
+		if(vertical != 0) {
+			body.rotation *= new Quaternion(Mathf.Sin(vertical), 0, 0, Mathf.Cos(vertical));
+		}
+		
+		if(horizontal != 0) {
+			body.rotation *= new Quaternion(0, Mathf.Sin(horizontal), 0, Mathf.Cos (horizontal));
+		}
+		
+		var qx = body.rotation.x;
+		var qy = body.rotation.y;
+		var qz = body.rotation.z;
+		var qw = body.rotation.w;
+		body.position = new Vector3(2 * (qy * qw + qz * qx) * radius, 2 * (qz * qy - qw * qx) * radius, ((qz * qz + qw * qw) - (qx * qx + qy * qy)) * radius);
+		body.rotation = Quaternion.FromToRotation(Vector3.up, body.position);
 	}
 }
