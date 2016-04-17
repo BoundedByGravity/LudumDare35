e45aab272a46bec1a713b787bc3272fc9c0e6868
VictorWinberg <dat14vwi@student.lu.se>
2016-04-17 13:53:51 +0200

    Life update

---
+++ Assets/Scripts/Life/Life.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Life : MonoBehaviour {
+
+	public abstract void interact ();
+
+	protected IEnumerator Popup(float time) {
+		Vector3 originalScale = transform.localScale;
+		transform.localScale = Vector3.zero;
+		float pastTime = 0;
+		while (pastTime < time) {
+			pastTime += Time.deltaTime;
+			transform.localScale = Vector3.Lerp (transform.localScale, originalScale, pastTime);
+			yield return null;
+		}
+	}
+}
---
+++ Assets/Scripts/Life/Tree.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree : Life {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Popup(1f));
+	}
+	
+	public override void interact() {
+		print ("Hi");
+	}
+}
