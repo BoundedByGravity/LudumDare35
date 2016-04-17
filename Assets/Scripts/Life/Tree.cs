64bba851a497eb4f282656b8f20b99271c92bad9
VictorWinberg <dat14vwi@student.lu.se>
2016-04-17 14:58:32 +0200

    Life added Tree

--- Assets/Scripts/Life/Tree.cs
+++ Assets/Scripts/Life/Tree.cs
@@ -8,10 +8,30 @@ public class Tree : Life {
 		StartCoroutine (Popup(1f));
 	}
 	
-	public override void interact() {
+	public override void interact(GameObject player) {
 		Transform[] allChildren = GetComponentsInChildren<Transform>();
 		foreach (Transform child in allChildren) {
-			child.parent = null;
+			child.parent = transform.parent;
+			if (child.name == "Tree") {
+				Rigidbody body = child.gameObject.AddComponent<Rigidbody> ();
+				body.AddExplosionForce (400f, player.transform.position, 5f);
+			} 
+			StartCoroutine (fadeOutAndDestroy (child.gameObject, 2f));
+		}
+	}
+
+	IEnumerator fadeOutAndDestroy(GameObject obj, float time) {
+		Destroy (obj, time);
+		Renderer render = obj.GetComponent<Renderer> ();
+		if (render != null) {
+			Color fromColor = render.material.color;
+			Color toColor = new Color (fromColor.r, fromColor.g, fromColor.b, 0);
+			float pastTime = 0;
+			while (pastTime < time) {
+				pastTime += Time.deltaTime;
+				render.material.color = Color.Lerp (fromColor, toColor, pastTime);
+				yield return null;
+			}
 		}
 	}
 }
