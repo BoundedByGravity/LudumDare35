36ec90fb0508fbdec8e70c5ac08671b21cf8bd0c
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-04-18 19:20:39 +0200

    Removed deprecated methods

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -24,27 +24,4 @@ public class Planet : MonoBehaviour {
 
 		mesh = core.GetComponent<MeshFilter> ().mesh;
 	}
-
-	[System.Obsolete("Use PopulatePlanet instead")]
-	public void Spawn(UnityEngine.Object obj, int number, float speed) {
-		StartCoroutine (Spawner (obj as GameObject, number, speed));
-	}
-
-	[System.Obsolete("Use PopulatePlanet instead")]
-	IEnumerator Spawner(Object obj, int number, float speed) {
-		while(number > 0) {
-			yield return new WaitForSeconds(speed);
-			Vector3 spawnPos = mesh.vertices [Random.Range(0, mesh.vertices.Length)] * core.transform.localScale.x * transform.localScale.x + core.transform.position;
-			GameObject clone = Instantiate (obj, spawnPos, Quaternion.FromToRotation(Vector3.up, spawnPos - transform.position)) as GameObject;
-			clone.transform.parent = holder.transform;
-			number--;
-			yield return null;
-		}
-	}
-
-	[System.Obsolete("Use Orbit instead")]
-	public void orbit() {
-		float modifier = Vector3.Distance(Vector3.zero, transform.position) * core.transform.localScale.x + transform.localScale.x + 1;
-		transform.RotateAround (Vector3.zero, Vector3.up, Time.deltaTime * 100f / modifier);
-	}
 }
